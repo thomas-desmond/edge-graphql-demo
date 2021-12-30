e35630a7a1fbbe1d37ffd1ac821a1d1197a84953
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2021-12-30 09:31:53 -0800

    Adding more example queries

--- graphQL/queries.ts
+++ graphQL/queries.ts
@@ -1,5 +1,6 @@
 import { gql } from "graphql-request";
 
+// Get all blogs
 export const GET_ALL_BLOGS = gql`
   {
     allM_Content_Blog {
@@ -9,4 +10,27 @@ export const GET_ALL_BLOGS = gql`
       }
     }
   }
+`
+
+// Get all blogs with "Next.js" in the title
+export const BLOGS_WITH_NEXTJS_TITLE = gql`
+{
+    allM_Content_Blog (where:{blog_Title_contains:"Next.js"} ) {
+    	results{
+        blog_Title
+        blog_Body
+      }
+    }
+}`
+
+// Get firs 3 blogs in Created On Descending order
+export const FIRST_THREE_BLOGS_DESC = gql`
+{
+    allM_Content_Blog (first:3 orderBy:CREATEDON_DESC) {
+    	results{
+        blog_Title
+        blog_Body
+      }
+    }
+}
 `
