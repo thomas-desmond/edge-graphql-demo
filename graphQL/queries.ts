c788379c5e72ea32156840331346a19ad97f71a6
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2022-01-13 09:25:50 -0800

    Added another sample query

--- graphQL/queries.ts
+++ graphQL/queries.ts
@@ -23,7 +23,7 @@ export const BLOGS_WITH_NEXTJS_TITLE = gql`
     }
 }`
 
-// Get firs 3 blogs in Created On Descending order
+// Get first 3 blogs in Created On Descending order
 export const FIRST_THREE_BLOGS_DESC = gql`
 {
     allM_Content_Blog (first:3 orderBy:CREATEDON_DESC) {
@@ -32,5 +32,19 @@ export const FIRST_THREE_BLOGS_DESC = gql`
         blog_Body
       }
     }
+}
+`
+
+// Get all blogs created between Dec 30th 2021 and Jan 14th 2022
+export const BLOGS_CREATED_ON_IN_DATE_RANGE = gql`
+{
+  allM_Content_Blog (where: {createdOn_gt:"2021-12-30", createdOn_lt: "2022-01-14"}) {
+    results {
+      blog_Title
+      blog_Body
+      id
+      createdOn
+    }
+  }
 }
 `
